2623c6018627727d4215b528a51347db16f99657
nat7216 <xukchen@ucm.es>
2023-07-28 14:49:45 +0200

    try

--- build.sh
+++ build.sh
@@ -16,11 +16,11 @@ cd $HOME/bsp && git clone https://github.com/Seeed-Studio/meta-seeed-reterminal.
 sed -i "s/usrmerge //" $HOME/meta-agl/meta-agl-core/conf/distro/poky-agl.conf
 cp -r $HOME/external/meta-agl-reterminal/conf/machine/seeed-reterminal/ $HOME/meta-agl/templates/machine/
 
-source $HOME/meta-agl/scripts/aglsetup.sh -b seeed-reterminal -m seeed-reterminal agl-demo agl-devel agl-flutter
+source $HOME/meta-agl/scripts/aglsetup.sh -b $MACHINE -m $MACHINE agl-demo agl-devel agl-flutter
 
 bitbake-layers add-layer $HOME/external/meta-tensorflow-lite
 bitbake-layers add-layer $HOME/bsp/meta-raspberrypi
-bitbake-layers add-layer $HOME/extenal/meta-agl-reterminal
+bitbake-layers add-layer $HOME/external/meta-agl-reterminal
 
-cat $HOME/extenal/meta-agl-reterminal/local.conf >> $HOME/$MACHINE/conf/local.conf
+cat $HOME/external/meta-agl-reterminal/local.conf >> $HOME/$MACHINE/conf/local.conf
 
